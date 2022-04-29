0e7f405459d7588f69167795cc561c03a2a36148
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-04-30 02:09:47 +0600

    footer added

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Home/Banner/Banner';
 import InventoryCards from '../Home/InventoryCards/InventoryCards';
+import Footer from '../Shared/Footer.js/Footer';
 
 const Header = () => {
     return (
         <div>
            <Banner/>
            <InventoryCards/>
+           <Footer/>
         </div>
     );
 };
