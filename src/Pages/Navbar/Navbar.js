6f4b7c23150d8c4ba4bd7e3197e356393ff3f3f6
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-01 00:55:15 +0600

    conditionally signout showed

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading/Loading';
 
 const Navbar = () => {
-
+    const [user, loading] = useAuthState(auth);
+    if (loading) {
+        return <Loading />
+    }
     return (
         <div className="w-full" >
             <header className="bg-teal-400">
@@ -16,10 +21,13 @@ const Navbar = () => {
                             <Link to="/blogs" className="block md:text-white mr-4" href="/link">Blogs</Link>
                             < Link to="/dashboard" className="block md:text-white mr-4" href="/link">Dashboard</Link>
                         </div>
-                        <div className="flex text-sm">
-                            <Link to="/login" className="p-2 ml-2 bg-white text-teal-500 font-semibold leading-none border border-gray-100 rounded hover:border-transparent hover:bg-gray-100" >Login</Link>
-                            <Link to="/signup" className="p-2 ml-2 bg-teal-500 text-gray-100 font-semibold leading-none border border-teal-600 rounded hover:border-transparent hover:bg-teal-600">Sign up</Link>
-                        </div>
+                        {
+                            user ?  <button to="/login" className="p-2 ml-2 bg-white text-teal-500 font-semibold leading-none border border-gray-100 rounded hover:border-transparent hover:bg-gray-100" >Sign Out</button> :
+                            <div className="flex text-sm">
+                                <Link to="/login" className="p-2 ml-2 bg-white text-teal-500 font-semibold leading-none border border-gray-100 rounded hover:border-transparent hover:bg-gray-100" >Login</Link>
+                                <Link to="/signup" className="p-2 ml-2 bg-teal-500 text-gray-100 font-semibold leading-none border border-teal-600 rounded hover:border-transparent hover:bg-teal-600">Sign up</Link>
+                            </div>
+                       }
                     </div>
                 </nav>
             </header>
