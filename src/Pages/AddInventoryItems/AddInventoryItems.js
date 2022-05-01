93bfa759723796fc7b88dab3b6a94f4b29c43c3f
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-01 20:47:15 +0600

    new item adde

--- src/Pages/AddInventoryItems/AddInventoryItems.js
+++ src/Pages/AddInventoryItems/AddInventoryItems.js
@@ -7,7 +7,7 @@ const AddInventoryItems = () => {
     const onSubmit = data => console.log(data);
     return (
         <>
-        <h3 className='text-center text-3xl my-1 text-green-400 underline'>Add Items</h3>
+        <h3 className='text-center text-3xl my-1 text-green-400 underline'>Add new products</h3>
             <div className='border-2 p-3 my-5 container sm:p-3 sm:w-full md:w-5/12 mx-auto bg-slate-300 rounded-xl'>
                 <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col bg-white rounded p-5">
                     <label className="block mb-2 text-sm font-bold text-gray-700">Product Name</label>
