9c20961f82ae74fd06a95bba15e2e48be48e64ef
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-02 02:51:54 +0600

    recent order data loaded

--- src/Pages/RecentTransactions/RecentTranscations.js
+++ src/Pages/RecentTransactions/RecentTranscations.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const RecentTranscations = () => {
+    const [transactions, setTransactions] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/transactions')
+            .then(res => res.json())
+            .then(data => setTransactions(data));
+    }, [])
+
     return (
         <div className='w-full mx-auto container'>
             <h2 className='text-2xl ml-6 mt-7 '><span className='text-lime-400 text-4xl'>Recent </span>Transactions</h2>
@@ -18,7 +25,7 @@ const RecentTranscations = () => {
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" class="px-6 py-3">
-                                Order-ID
+                                Order-ID {transactions.length}
                             </th>
                             <th scope="col" class="px-6 py-3">
                                 ITEM
@@ -34,9 +41,6 @@ const RecentTranscations = () => {
                                 PRICE
                             </th>
                             <th scope="col" class="px-6 py-3">
-                                CATEGORY
-                            </th>
-                            <th scope="col" class="px-6 py-3">
                                 BRAND NAME
                             </th>
                             <th scope="col" class="px-6 py-3">
@@ -45,32 +49,32 @@ const RecentTranscations = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td class="px-6 py-4">
-                                Sliver
-                            </td>
-                            <td class="px-6 py-4">
-                                Laptop
-                            </td>
-                            <td class="px-6 py-4">
-                                $2999
-                            </td>
-                            <td class="px-6 py-4">
-                                $2999
-                            </td>
-                            <td class="px-6 py-4">
-                                $2999
-                            </td>
-                            <td class="px-6 py-4">
-                                $2999
-                            </td>
-                            <td class="px-6 py-4">
-                               <p className='bg-lime-300 text-center py-1 rounded'>Proceed</p>
-                            </td>
-                        </tr>
+                        {
+                            transactions.map(transaction =>
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                    {transaction.OrderId}
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        {transaction.item}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {transaction.dealerName}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {transaction.paymentInfo}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                       {transaction.price}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                       {transaction.brand}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <p className='bg-lime-300 text-center py-1 rounded'> {transaction.status}</p>
+                                    </td>
+                                </tr>
+                            )}
                     </tbody>
                 </table>
             </div>
