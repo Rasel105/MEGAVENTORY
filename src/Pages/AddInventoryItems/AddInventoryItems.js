9be9e04189d9b8ee62a571135285b3327226d646
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-02 17:55:55 +0600

    my item page created and gave design and fetch data and showed data

--- src/Pages/AddInventoryItems/AddInventoryItems.js
+++ src/Pages/AddInventoryItems/AddInventoryItems.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import { BiAddToQueue } from "react-icons/bi";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import auth from '../../firebase.init';
 
 
 const AddInventoryItems = () => {
+    const [user] = useAuthState(auth);
+    const email = user.email;
+
     const { register, handleSubmit } = useForm();
     const onSubmit = (data, e) => {
         fetch('http://localhost:5000/product', {
@@ -31,18 +36,20 @@ const AddInventoryItems = () => {
             <h3 className='text-center text-3xl my-1 text-green-400 underline'>Add new products</h3>
             <div className='border-2 p-3 my-5 container sm:p-3 sm:w-full md:w-5/12 mx-auto bg-slate-300 rounded-xl'>
                 <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col bg-white rounded p-5">
+                    <label className="block mb-2 text-sm font-bold text-gray-700">Email</label>
+                    <input className='mb-2 py-2 px-2 text-lg shadow-lg text-gray-700 border rounded-lg appearance-none bg-gray-200 focus:outline-none focus:shadow-outline ' value={email} readOnly disabled {...register("email")} />
                     <label className="block mb-2 text-sm font-bold text-gray-700">Product Name</label>
                     <input className='mb-2 py-2 px-2 text-lg shadow-lg text-gray-700 border rounded-lg appearance-none focus:outline-none focus:shadow-outline ' placeholder='Product Name' {...register("product_name", { required: true })} />
                     <label className="block mb-2 text-sm font-bold  text-gray-700">Price</label>
-                    <input className='mb-2 py-2 px-2 text-lg shadow-lg text-gray-700 border rounded-lg appearance-none focus:outline-none focus:shadow-outline ' placeholder='Price' type="number" {...register("price")} />
+                    <input className='mb-2 py-2 px-2 text-lg shadow-lg text-gray-700 border rounded-lg appearance-none focus:outline-none focus:shadow-outline ' placeholder='Price' type="number" {...register("price", { required: true })} />
                     <label className="block mb-2 text-sm font-bold text-gray-700">Quntity</label>
-                    <input className='mb-2 py-2 px-2 text-lg shadow-lg text-gray-700 border rounded-lg appearance-none focus:outline-none focus:shadow-outline ' placeholder='Quntity' type="number" {...register("quantity")} />
+                    <input className='mb-2 py-2 px-2 text-lg shadow-lg text-gray-700 border rounded-lg appearance-none focus:outline-none focus:shadow-outline ' placeholder='Quntity' type="number" {...register("quantity", { required: true })} />
                     <label className="block mb-2 text-sm font-bold text-gray-700">Supplier</label>
-                    <input className='mb-2 py-2 px-2 text-lg shadow-lg text-gray-700 border rounded-lg appearance-none focus:outline-none focus:shadow-outline ' type="text" placeholder='Supplier' {...register("supplier_name")} />
+                    <input className='mb-2 py-2 px-2 text-lg shadow-lg text-gray-700 border rounded-lg appearance-none focus:outline-none focus:shadow-outline ' type="text" placeholder='Supplier' {...register("supplier_name", { required: true })} />
                     <label className="block mb-2 text-sm font-bold text-gray-700">Img URL</label>
-                    <input className='mb-2 py-2 px-2 text-lg shadow-lg text-gray-700 border rounded-lg appearance-none focus:outline-none focus:shadow-outline ' type="text" placeholder='Image URL' {...register("img")} />
+                    <input className='mb-2 py-2 px-2 text-lg shadow-lg text-gray-700 border rounded-lg appearance-none focus:outline-none focus:shadow-outline ' type="text" placeholder='Image URL' {...register("img", { required: true })} />
                     <label className="block mb-2 text-sm font-bold text-gray-700">Description</label>
-                    <textarea className='mb-2 py-2 px-2 text-lg shadow-lg text-gray-700 border rounded-lg appearance-none focus:outline-none focus:shadow-outline ' placeholder='Description' type="text" {...register("description")} />
+                    <textarea className='mb-2 py-2 px-2 text-lg shadow-lg text-gray-700 border rounded-lg appearance-none focus:outline-none focus:shadow-outline ' placeholdegir='Description' type="text" {...register("description", { required: true })} />
                     <div className='flex justify-end'>
                         <button className='bg-lime-500 py-2 px-4 mt-2 rounded-lg text-xl mx-2 text-white' type='submit'>
                             Add
--- src/Pages/MyItems/MyItems.js
+++ src/Pages/MyItems/MyItems.js
@@ -4,7 +4,7 @@ import auth from '../../firebase.init.js'
 
 const MyItems = () => {
     const [user] = useAuthState(auth);
-    const [myItemS, setMyItems] = useState('');
+    const [myItemS, setMyItems] = useState([]);
 
     useEffect(() => {
         const email = user.email;
@@ -17,11 +17,58 @@ const MyItems = () => {
             });
     }, [user]);
 
+    const handleProductDelete = (id) => {
 
+    }
 
     return (
-        <div>
-            <h2>My Items {myItemS.length}</h2>
+        <div className=''>
+            <h2 className='text-4xl mx-8 text-center my-2'>My <span className='text-green-400 '>Items {myItemS.length}</span></h2>
+            <div className="container sm:w-full md:w-1/2 mx-auto my-5  overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                Product name
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Price
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Quntitry
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Supplier
+                            </th>
+                            <th scope='col' className='px-6 py-3'>
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    {
+                        myItemS.map(item => <tbody>
+                            <tr key={item._id} className="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                    {item.product_name}
+                                </th>
+                                <td className="px-6 py-4">
+                                    ${item.price}
+                                </td>
+                                <td className="px-6 py-4">
+                                    {item.quantity}
+                                </td>
+                                <td className="px-6 py-4">
+                                    {item.supplier_name}
+                                </td>
+                                <td className="px-6 py-4">
+                                    <button onClick={() => handleProductDelete(item._id)} href="#" className="font-medium bg-red-400 py-2 px-3 rounded text-white">Delete</button>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                        )}
+                </table>
+            </div>
         </div>
     );
 };
