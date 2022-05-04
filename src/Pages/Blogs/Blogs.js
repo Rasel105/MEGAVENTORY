d081ab43e86e9b71f155d598e0e99f7cb1620088
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-05 02:08:34 +0600

    blog section done

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,51 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+    useEffect(() => {
+        fetch('https://thawing-everglades-09724.herokuapp.com/blogs')
+            .then(res => res.json())
+            .then(data => {
+                setBlogs(data);
+            });
+    }, []);
     return (
         <>
             <div className='grid md:grid-cols-2 gap-5 px-5 sm:grid-cols-1'>
-                <div className="max-w-sm mx-auto items-center justify-center bg-white rdounde-lg border rounded-lg my-2 border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <div className="p-5">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-700 dark:text-white">Title</h5>
-                        <div className='flex justify-between items-center'>
-                            <p>
-                                Javasctipt: Javascript is a programming Language that is used for writting  script on the website
-                                Node: Node JS is a runtime environment.
-                                Javascript: Javasctript is just run in the browser.
-                                Node: Node use server side.
-                                Javascript: Javascript can perform 
-                                Node: V8 is the Javascript engine inside of node.
-                                JS: Javascript is used in frontend development.
-                                Node: Nodejs is written in C, C++ and Javascript.   
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="max-w-sm mx-auto items-center justify-center bg-white rdounde-lg border rounded-lg my-2 border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                    <div className="p-5">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-700 dark:text-white">Title</h5>
-                        <div className='flex justify-between items-center'>
-                            <p>
-                                Javasctipt: Javascript is a programming Language that is used for writting  script on the website
-                                <br />
-                                Node: Node JS is a runtime environment.
-                                <br />
-                                Javascript: Javasctript is just run in the browser.
-                                <br />
-                                Node: Node use server side.
-                                <br />
-                                Javascript: Javascript can perform 
-                                <br />
-                                Node: V8 is the Javascript engine inside of node.
-                                <br />
-                                JS: Javascript is used in frontend development.
-                                Node: Nodejs is written in C, C++ and Javascript.   
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                {
+                    blogs.map(blog =>
+                        <div className="max-w-sm mx-auto items-center justify-center bg-white rdounde-lg border rounded-lg my-2 border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-5">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-700 dark:text-white">{blog.title}?</h5>
+                                <div className='flex justify-between items-center'>
+                                    <p>
+                                        {blog.blogdetails}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>)
+                }
             </div>
-
         </>
     );
 };
