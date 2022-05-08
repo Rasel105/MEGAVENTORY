31a5d58b3071ade0f42852e1cff7c0a44dee9345
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-08 07:43:41 +0600

    banner section animation added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 import banner from '../../../images/banner1.jpg'
 
 const Banner = () => {
     return (
         <div className="relative w-full overflow-hidden">
             <div className="active relative w-full">
-                <img
-                    src={banner}
-                    className="block w-full md:h-screen"
-                    alt=""
-                />
+                <Fade right>
+                    <img
+                        src={banner}
+                        className="block w-full md:h-screen"
+                        alt=""
+                    />
+                </Fade>
             </div>
 
         </div>
