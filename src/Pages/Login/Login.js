0d47e97f42a10b24c32f02a4da104275ab0d9db9
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-08 07:48:44 +0600

    login page animation added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -7,6 +7,7 @@ import SocialLogIn from '../Shared/SocialLogIn/SocialLogIn';
 import loginImage from '../../images/formImages/login.jpg';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Fade from 'react-reveal/Fade';
 
 const Login = () => {
     const navigate = useNavigate();
@@ -92,71 +93,72 @@ const Login = () => {
 
     return (
         <div className="container mx-auto min-h-screen">
-            <div className="flex justify-center px-6 my-2">
-                <div className="w-full xl:w-3/4 lg:w-7 flex">
-                    <div className="w-full h-auto hidden lg:block lg:w-1/2 bg-cover rounded-l-lg">
-                        <img src={loginImage} alt="" />
-                    </div>
-                    <div className="w-full lg:w-1/2 bg-gray-50 p-5 shadow-2xl rounded-lg lg:rounded-l-none">
-                        <h3 className="mb-2 text-2xl text-center">Welcome Back!</h3>
-                        <form onSubmit={handleLogin} className="px-8 pt-3 pb-8 mb-4 bg-white rounded">
-                            <div className="mb-4">
-                                <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="username">
-                                    Email <span className='text-red-600'>*</span>
-                                </label>
-                                <input
-                                    className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                    name="email"
-                                    ref={emailRef}
-                                    type="email"
-                                    required
-                                    placeholder="Your email"
-                                />
-                            </div>
-                            <div className="mb-2">
-                                <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="password">
-                                    Password <span className='text-red-600'>*</span>
-                                </label>
-                                <input
-                                    className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                                    name="password"
-                                    type="password"
-                                    required
-                                    placeholder="******************"
-                                />
-                            </div>
-                            <div className="mb-7 flex justify-between">
-                                <div>
-                                    <input className="mr-2 leading-tight" type="checkbox" id="checkbox_id" />
-                                    <label className="text-sm">
-                                        Remember Me
+            <Fade left>
+                <div className="flex justify-center px-6 my-2">
+                    <div className="w-full xl:w-3/4 lg:w-7 flex">
+                        <div className="w-full h-auto hidden lg:block lg:w-1/2 bg-cover rounded-l-lg">
+                            <img src={loginImage} alt="" />
+                        </div>
+                        <div className="w-full lg:w-1/2 bg-gray-50 p-5 shadow-2xl rounded-lg lg:rounded-l-none">
+                            <h3 className="mb-2 text-2xl text-center">Welcome Back!</h3>
+                            <form onSubmit={handleLogin} className="px-8 pt-3 pb-8 mb-4 bg-white rounded">
+                                <div className="mb-4">
+                                    <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="username">
+                                        Email <span className='text-red-600'>*</span>
                                     </label>
+                                    <input
+                                        className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                        name="email"
+                                        ref={emailRef}
+                                        type="email"
+                                        required
+                                        placeholder="Your email"
+                                    />
                                 </div>
-                                <div className="text-center">
+                                <div className="mb-2">
+                                    <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="password">
+                                        Password <span className='text-red-600'>*</span>
+                                    </label>
+                                    <input
+                                        className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                        name="password"
+                                        type="password"
+                                        required
+                                        placeholder="******************"
+                                    />
+                                </div>
+                                <div className="mb-7 flex justify-between">
+                                    <div>
+                                        <input className="mr-2 leading-tight" type="checkbox" id="checkbox_id" />
+                                        <label className="text-sm">
+                                            Remember Me
+                                        </label>
+                                    </div>
+                                    <div className="text-center">
+                                        <button
+                                            onClick={resetPassword}
+                                            className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+                                        >
+                                            Forgot Password?
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className="mb-3 text-center">
+                                    {/* <p className='text-red-700 mb-2'>{error?.message}</p> */}
                                     <button
-                                        onClick={resetPassword}
-                                        className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+                                        className="w-full px-4 py-2 font-bold text-white bg-green-400 rounded-full hover:bg-green-600 focus:outline-none focus:shadow-outline"
+                                        type="submit"
                                     >
-                                        Forgot Password?
+                                        Login
                                     </button>
                                 </div>
-                            </div>
-                            <div className="mb-3 text-center">
-                                {/* <p className='text-red-700 mb-2'>{error?.message}</p> */}
-                                <button
-                                    className="w-full px-4 py-2 font-bold text-white bg-green-400 rounded-full hover:bg-green-600 focus:outline-none focus:shadow-outline"
-                                    type="submit"
-                                >
-                                    Login
-                                </button>
-                            </div>
-                            <div className='flex items-center justify-center my-2'>
-                                <div className='border-2 w-1/2'></div>
-                                <p className='text-center mx-2'>or</p>
-                                <div className='border-2 w-1/2'></div>
-                            </div>
+                                <div className='flex items-center justify-center my-2'>
+                                    <div className='border-2 w-1/2'></div>
+                                    <p className='text-center mx-2'>or</p>
+                                    <div className='border-2 w-1/2'></div>
+                                </div>
 
-                            {/* <div className="text-center mb-3">
+                                {/* <div className="text-center mb-3">
                                 <button
                                     className="w-full flex justify-evenly items-center px-4 py-2 font-bold text-black rounded-full border-2 bg-white focus:outline-none focus:shadow-outline"
                                     type="button">
@@ -164,21 +166,22 @@ const Login = () => {
                                     <span className='text-center'>Continue with google</span>
                                 </button>
                             </div> */}
-                            <SocialLogIn />
+                                <SocialLogIn />
 
-                            <div className="mb-2 text-center">
-                                <Link to="/signup"
-                                    className="w-full px-4 py-2 font-bold text-white bg-green-400 rounded-full hover:bg-green-600 focus:outline-none focus:shadow-outline"
-                                    type="button"
-                                >
-                                    Create an Account
-                                </Link>
-                            </div>
-                        </form>
-                        <ToastContainer />
+                                <div className="mb-2 text-center">
+                                    <Link to="/signup"
+                                        className="w-full px-4 py-2 font-bold text-white bg-green-400 rounded-full hover:bg-green-600 focus:outline-none focus:shadow-outline"
+                                        type="button"
+                                    >
+                                        Create an Account
+                                    </Link>
+                                </div>
+                            </form>
+                            <ToastContainer />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Fade>
         </div>
     )
 };
